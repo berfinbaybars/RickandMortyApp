b601940788032e547ee7ff861579d6a0f51fa70d
Berfin Baybars <berfinbaybars@hotmail.com>
2022-09-20 09:39:50 +0300

    App.js setup

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
+import 'react-native-reanimated';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Navbar from './src/components//layout/Navbar';
+import LocationListing from './src/pages/LocationListing';
+import store from './src/redux/store';
+import styles from './src/styles/main-styles';
+import LocationDetail from './src/pages/LocationDetail';
+import CharacterDetail from './src/pages/CharacterDetail';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Provider store={store}>
+            <SafeAreaView style={styles.body}>
+                <StatusBar style="auto" />
+                <NavigationContainer>
+                    <Stack.Navigator
+                        screenOptions={{
+                            headerTitle: (props) => <Navbar {...props} />,
+                            headerTitleAlign: 'center',
+                        }}
+                    >
+                        <Stack.Screen name="Home" component={LocationListing} />
+                        <Stack.Screen name="LocationDetail" component={LocationDetail} />
+                        <Stack.Screen name="CharacterDetail" component={CharacterDetail} />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </SafeAreaView>
+        </Provider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
